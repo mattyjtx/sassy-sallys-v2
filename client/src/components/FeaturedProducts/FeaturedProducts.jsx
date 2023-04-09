4745ce6cddb21ae90e926d86fadc17598e3a3db4
Matthew Gonzales <mattgonzales3@yahoo.com>
2023-04-08 19:03:49 -0500

    adding few more images for featuredProducts

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -17,7 +17,7 @@ const FeaturedProducts = ({type}) => {
         {
             id: 1,
             img:"https://images.pexels.com/photos/3538028/pexels-photo-3538028.jpeg?auto=compress&cs=tinysrgb&w=600",
-            img2:"",
+            img2:"https://images.pexels.com/photos/2866119/pexels-photo-2866119.jpeg?auto=compress&cs=tinysrgb&w=800",
             title: "Coat",
             isNew: true,
             oldprice: 19,
@@ -26,7 +26,7 @@ const FeaturedProducts = ({type}) => {
         {
             id: 1,
             img:"https://images.pexels.com/photos/1007018/pexels-photo-1007018.jpeg?auto=compress&cs=tinysrgb&w=600",
-            img2:"",
+            img2:"https://images.pexels.com/photos/1007017/pexels-photo-1007017.jpeg?auto=compress&cs=tinysrgb&w=800",
             title: "Skirt",
             isNew: true,
             oldprice: 19,
@@ -35,7 +35,7 @@ const FeaturedProducts = ({type}) => {
         {
             id: 1,
             img:"https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&w=600",
-            img2:"",
+            img2:"https://images.pexels.com/photos/16122227/pexels-photo-16122227.jpeg?auto=compress&cs=tinysrgb&w=800",
             title: "Hat",
             isNew: true,
             oldprice: 19,
