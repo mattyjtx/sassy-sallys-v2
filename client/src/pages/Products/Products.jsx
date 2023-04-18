73ef25b45755b96891a2df1f5d31991c67ee2cd1
Matthew Gonzales <mattgonzales3@yahoo.com>
2023-04-17 23:43:24 -0500

    finishing left side for Products.jsx

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -3,7 +3,45 @@ import "./Products.scss"
 
 const Products = () => {
   return (
-    <div className='products'>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id='1' value={1}/>
+            <label htmlFor='1'>shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='2' value={2}/>
+            <label htmlFor='2'>skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='3' value={3}/>
+            <label htmlFor='3'>coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id='asc' value="asc" name='price'/>
+            <label htmlFor='asc'>Price (lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id='asc' value="asc" name='price'/>
+            <label htmlFor='asc'>Price (highest first)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 
